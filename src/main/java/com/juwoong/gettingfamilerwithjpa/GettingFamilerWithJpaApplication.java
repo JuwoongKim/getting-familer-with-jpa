9ea0a2fe9faea3cb17e37899bddaaec50d0deddb
juwoong kim <wwong7247@gmail.com>
2024-01-30 16:28:44 +0900

    학습용 프로젝트 초기화

---
+++ src/main/java/com/juwoong/gettingfamilerwithjpa/GettingFamilerWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.juwoong.gettingfamilerwithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingFamilerWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingFamilerWithJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juwoong/gettingfamilerwithjpa/GettingFamilerWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juwoong.gettingfamilerwithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingFamilerWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
